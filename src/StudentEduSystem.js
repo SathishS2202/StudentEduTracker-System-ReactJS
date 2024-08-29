e10de91ee86fcc554bdc4d27928eaa1324894ca4
Sathish S <143690390+SathishS2202@users.noreply.github.com>
2024-08-29 22:01:58 +0530

    Update StudentEduSystem.js

--- src/StudentEduSystem.js
+++ src/StudentEduSystem.js
@@ -7,11 +7,18 @@ import { TbSettingsStar } from "react-icons/tb";
 import { HiOutlineViewGridAdd } from "react-icons/hi";
 import { BiFileBlank } from "react-icons/bi";
 import { RiContactsFill } from "react-icons/ri";
+import { FaUserAlt } from "react-icons/fa";
+import { FaBook } from "react-icons/fa";
+import { FaFileAlt } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+import { FaClinicMedical } from "react-icons/fa";
+import { FaBookReader } from "react-icons/fa";
+import { FaStreetView } from "react-icons/fa";
 function StudentManagementSystem()
 {
     return(
         <div className="body">
-            <div className="gridcontainer">
+            <div className="gridcontainer " style={{height:'250vh'}}>
                 <ul>
                     <li> <img src={Hicet1} alt='hicet'></img></li>
                     <li className="icons"><IoMdContacts /></li>
@@ -39,21 +46,44 @@ function StudentManagementSystem()
               </div>
               <div className="violet">
                 <ul>
-                   <li className="icon"><RiContactsFill /></li>
+                   <li className="icon"><FaUserAlt /></li>
                    <li className="item">Regular Students</li>
                 </ul>  
               </div>
               <div className="green">
                 <ul>
-                   <li className="icon"><RiContactsFill /></li>
+                   <li className="icon"><FaStreetView /></li>
                    <li className="item">Regular Students</li>
                 </ul>  
               </div>
-
             </div>
+            <hr/>
+                 <h1>Menu</h1>
+                <div className="menucontainer">
+                    <div className="menu">
+                         <p className="menuicons"><FaUserAlt /></p>
+                          <p>Regular Enrollment</p>
+                      </div>
+                    <div className="menu"><p className="menuicons"><FaBook />
+                    </p><p>Remedial Enrollment</p></div>
+                    <div className="menu">
+                      <p className="menuicons"><FaStreetView /></p>
+                      <p>Club Management</p>
+                    </div>
+                    <div className="menu">
+                      <p className="menuicons"><FaBookReader /></p>
+                      <p>Classroom Management</p></div>
+                    <div className="menu"><p  className="menuicons"><MdEmail /></p>
+                      <p>SMS/Email</p></div>
+                    <div className="menu">
+                      <p className="menuicons"><FaFileAlt /></p>
+                      <p>Attendance</p>
+                      </div>
+                    <div className="menu">
+                      <p className="menuicons"><FaClinicMedical/></p>
+                      <p>Clinic</p></div>
+                </div>
             </div>
-            
-        
         </div>
     )
 }
